336437e8a8f9e7df7e86d3ede52d58ece6ced896
lly <2267996327@qq.com>
2018-06-15 15:11:55 +0800

    ss

--- src/main/java/devops/demo/TestController/Service.java
+++ src/main/java/devops/demo/TestController/Service.java
@@ -11,6 +11,8 @@ public class Service
     public static void main(String[] args)
     {
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
 
     }
 }
